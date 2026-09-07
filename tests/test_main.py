24cfac4ee7e11de56817b2dbce8fdf0386d70fe4
Rachanon tanee <NonzaSKD@gmail.com>
2026-09-07 16:43:29 +0700

    Initial commit

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_hello():
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Hello Jenkins"
+    }
